17df8858b3da6b1c0922ccbc4e1087629fb44bbe
Eli Valenti <AmaroqOkami@users.noreply.github.com>
2017-09-11 11:21:20 -0400

    -Reworked settings code to be more efficient and easier to add elements to.

--- ExtendedBuildings/ExtendedBuildingsMod.cs
+++ ExtendedBuildings/ExtendedBuildingsMod.cs
@@ -1,11 +1,4 @@
 ﻿using ICities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml;
-using System.Xml.Serialization;
-using System.IO;
 
 namespace ExtendedBuildings
 {
@@ -26,97 +19,25 @@ namespace ExtendedBuildings
             }
         }
 
-        private void writeXmlSettings()
+
+        public void OnSettingsUI(UIHelperBase helper)
         {
-            XmlWriterSettings settings = new XmlWriterSettings();
-            settings.Indent = true;
-            settings.IndentChars = "\t";
-            settings.NewLineChars = "\n";
-
-            XmlWriter xmlWriter = XmlWriter.Create("ExtendedBuildingUISettings.xml", settings);
-
-            xmlWriter.WriteStartDocument();
-            xmlWriter.WriteStartElement("settings");
-
-            foreach (KeyValuePair<string, object> entry in userSettings)
-            {
-                xmlWriter.WriteStartElement(entry.Key);
-                xmlWriter.WriteString(entry.Value.ToString());
-                xmlWriter.WriteEndElement();
-            }
-
-            xmlWriter.WriteEndElement();
-            xmlWriter.WriteEndDocument();
-            xmlWriter.Close();
+            helper.AddCheckbox("Enable random building names", EB_Options.Instance.enableNames, BuildingNameCheck);
+            helper.AddCheckbox("Enable random building descriptions", EB_Options.Instance.enableDescriptions, BuildingDescCheck);
         }
 
         private void BuildingNameCheck(bool enableName)
         {
-            //BuildingInfoWindow5.ShowName = enableName;
+            EB_Options.Instance.enableNames = enableName;
+            EB_Options.Instance.Save();
 
-            userSettings["enableNames"] = enableName;
-            writeXmlSettings();
         }
 
         private void BuildingDescCheck(bool enableDesc)
         {
-            //BuildingInfoWindow5.ShowDescription = enableDesc;
+            EB_Options.Instance.enableDescriptions = enableDesc;
+            EB_Options.Instance.Save();
 
-            userSettings["enableDescriptions"] = enableDesc;
-            writeXmlSettings();
-        }
-
-        public static Dictionary<string, object> userSettings = new Dictionary<string, object>();
-        public void OnSettingsUI(UIHelperBase helper)
-        {
-            //XML initial load or creation
-            if (userSettings.Count <= 0)
-            {
-                try
-                {
-                    XmlDocument xmlDoc = new XmlDocument();
-                    xmlDoc.Load("ExtendedBuildingUISettings.xml");
-
-                    bool nameEnable = Convert.ToBoolean(xmlDoc.SelectSingleNode("settings/enableNames").InnerText);
-                    //BuildingInfoWindow5.ShowName = nameEnable;
-                    userSettings.Add("enableNames", nameEnable);
-
-                    bool descEnable = Convert.ToBoolean(xmlDoc.SelectSingleNode("settings/enableDescriptions").InnerText);
-                    //BuildingInfoWindow5.ShowDescription = descEnable;
-                    userSettings.Add("enableDescriptions", descEnable);
-                }
-                catch (FileNotFoundException)
-                {
-                    XmlWriterSettings settings = new XmlWriterSettings();
-                    settings.Indent = true;
-                    settings.IndentChars = "\t";
-                    settings.NewLineChars = "\n";
-
-                    XmlWriter xmlWriter = XmlWriter.Create("ExtendedBuildingUISettings.xml", settings);
-
-                    xmlWriter.WriteStartDocument();
-                    xmlWriter.WriteStartElement("settings");
-
-                    xmlWriter.WriteStartElement("enableNames");
-                    xmlWriter.WriteString("True");
-                    xmlWriter.WriteEndElement();
-                    userSettings.Add("enableNames", true);
-
-                    xmlWriter.WriteStartElement("enableDescriptions");
-                    xmlWriter.WriteString("True");
-                    xmlWriter.WriteEndElement();
-                    userSettings.Add("enableDescriptions", true);
-
-                    xmlWriter.WriteEndElement();
-                    xmlWriter.WriteEndDocument();
-                    xmlWriter.Close();
-                }
-            }
-
-            UIHelperBase group = helper.AddGroup("Extended Building Info");
-
-            group.AddCheckbox("Enable random building names", (bool) userSettings["enableNames"], BuildingNameCheck);
-            group.AddCheckbox("Enable random building descriptions", (bool) userSettings["enableDescriptions"], BuildingDescCheck);
         }
     }
 }
---
+++ ExtendedBuildings/Options.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using ColossalFramework.Plugins;
+
+namespace ExtendedBuildings
+{
+    public class EB_Options
+    {
+        private const string optionsFileName = "ExtendedBuildingInfoOptions.xml";
+        private static EB_Options _instance;
+
+        public static EB_Options Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = CreateFromFile();
+                }
+
+                return _instance;
+            }
+        }
+
+        //All option variables get added here.
+        public bool enableNames;
+        public bool enableDescriptions;
+
+        public EB_Options()
+        {
+            enableNames = false;
+            enableDescriptions = false;
+        }
+
+
+        public void Save()
+        {
+            try
+            {
+                XmlSerializer ser = new XmlSerializer(typeof(EB_Options));
+                TextWriter writer = new StreamWriter(optionsFileName);
+                ser.Serialize(writer, this);
+                writer.Close();
+            }
+            catch (Exception ex)
+            {
+                DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "Ex. Building Info: " + ex.Message);
+            }
+        }
+
+        public static EB_Options CreateFromFile()
+        {
+            if (!File.Exists(optionsFileName)) return new EB_Options();
+
+            try
+            {
+                XmlSerializer ser = new XmlSerializer(typeof(EB_Options));
+                TextReader reader = new StreamReader(optionsFileName);
+                EB_Options instance = (EB_Options)ser.Deserialize(reader);
+                reader.Close();
+
+                return instance;
+            }
+            catch (Exception ex)
+            {
+                DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "Ex. Building Info: " + ex.Message);
+
+                return new EB_Options();
+            }
+        }
+    }
+}
