b8d74ecd52ab37146d8e59afec1fa58a5ae5f822
John Rayes <live627@gmail.com>
2019-09-28 09:13:21 -0700

    modoconfig

---
+++ ExtendedBuildings/ExtendedBuildings.cs
@@ -0,0 +1,44 @@
+﻿using ICities;
+using System;
+
+namespace ExtendedBuildings
+{
+    public class ExtendedBuildings : IUserMod
+    {
+        public string Name => "Extended Building Information";
+
+        public string Description => "Displays level up requirements, and an optional random bulding name/company description.";
+
+        public void OnSettingsUI(UIHelperBase helper)
+        {
+            UIHelperBase group = helper.AddGroup(Name);
+            
+            group.AddDropdown("Display", Enum.GetNames(typeof(ModConfig.DisplayCategory)), (int)ModConfig.Instance.Display, sel =>
+            {
+                ModConfig.Instance.Display = (ModConfig.DisplayCategory)sel;
+                ModConfig.Instance.Save();
+            });
+
+            float selectedValue = ModConfig.Instance.Opacity;
+            group.AddSlider("Opacity", 0.5f, 1f, 0.05f, selectedValue, sel =>
+            {
+                ModConfig.Instance.Opacity = sel;
+                ModConfig.Instance.Save();
+            });
+            helper.AddCheckbox("Enable random building names", ModConfig.Instance.EnableNames, BuildingNameCheck);
+            helper.AddCheckbox("Enable random building descriptions", ModConfig.Instance.EnableDescriptions, BuildingDescCheck);
+        }
+
+        private void BuildingNameCheck(bool enableName)
+        {
+            ModConfig.Instance.EnableNames = enableName;
+            ModConfig.Instance.Save();
+        }
+
+        private void BuildingDescCheck(bool enableDesc)
+        {
+            ModConfig.Instance.EnableDescriptions = enableDesc;
+            ModConfig.Instance.Save();
+        }
+    }
+}
---
+++ ExtendedBuildings/ModConfig.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using ColossalFramework.Plugins;
+
+namespace ExtendedBuildings
+{
+    public class ModConfig
+    {
+        public enum DisplayCategory { Compact, Extended };
+        private const string optionsFileName = "ExtendedBuildingInfoOptions.xml";
+        private static ModConfig _instance;
+
+        public static ModConfig Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = CreateFromFile();
+
+                return _instance;
+            }
+        }
+
+        public float Opacity { get; internal set; } = 0.85f;
+        public DisplayCategory Display { get; internal set; } = DisplayCategory.Compact;
+        public bool EnableNames { get; internal set; } = true;
+        public bool EnableDescriptions { get; internal set; } = true;
+
+        public void Save()
+        {
+            try
+            {
+                XmlSerializer ser = new XmlSerializer(typeof(ModConfig));
+                using (TextWriter writer = new StreamWriter(optionsFileName))
+                {
+                    ser.Serialize(writer, this);
+                }
+            }
+            catch (Exception ex)
+            {
+                DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "Ex. Building Info: " + ex.Message);
+            }
+        }
+
+        public static ModConfig CreateFromFile()
+        {
+            if (!File.Exists(optionsFileName))
+                return new ModConfig();
+
+            try
+            {
+                XmlSerializer ser = new XmlSerializer(typeof(ModConfig));
+                using (TextReader reader = new StreamReader(optionsFileName))
+                {
+                    ModConfig instance = (ModConfig)ser.Deserialize(reader);
+
+                    return instance;
+                }
+            }
+            catch (Exception ex)
+            {
+                DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "Ex. Building Info: " + ex.Message);
+
+                return new ModConfig();
+            }
+        }
+    }
+}
