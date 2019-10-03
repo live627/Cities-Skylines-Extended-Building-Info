406eb23c8685df7a0885ba10d4fd117820cf4018
John Rayes <live627@gmail.com>
2019-10-02 20:59:21 -0700

    shgow mod version number in options page header

--- ExtendedBuildings/ExtendedBuildings.cs
+++ ExtendedBuildings/ExtendedBuildings.cs
@@ -5,40 +5,33 @@ namespace ExtendedBuildings
 {
     public class ExtendedBuildings : IUserMod
     {
-        public string Name => "Extended Building Information";
+        public string Name => "Extended Building Info";
 
         public string Description => "Displays level up requirements, and an optional random bulding name/company description.";
 
         public void OnSettingsUI(UIHelperBase helper)
         {
-            UIHelperBase group = helper.AddGroup(Name);
-            
+            UIHelperBase group = helper.AddGroup(String.Format("{0} (v{1})", Name, typeof(Localization).Assembly.GetName().Version));
             group.AddDropdown("Display", Enum.GetNames(typeof(ModConfig.DisplayCategory)), (int)ModConfig.Instance.Display, sel =>
             {
                 ModConfig.Instance.Display = (ModConfig.DisplayCategory)sel;
                 ModConfig.Instance.Save();
             });
-
-            float selectedValue = ModConfig.Instance.Opacity;
-            group.AddSlider("Opacity", 0.5f, 1f, 0.05f, selectedValue, sel =>
+            group.AddSlider("Opacity", 0.5f, 1f, 0.05f, ModConfig.Instance.Opacity, sel =>
             {
                 ModConfig.Instance.Opacity = sel;
                 ModConfig.Instance.Save();
             });
-            helper.AddCheckbox("Enable random building names", ModConfig.Instance.EnableNames, BuildingNameCheck);
-            helper.AddCheckbox("Enable random building descriptions", ModConfig.Instance.EnableDescriptions, BuildingDescCheck);
-        }
-
-        private void BuildingNameCheck(bool enableName)
-        {
-            ModConfig.Instance.EnableNames = enableName;
-            ModConfig.Instance.Save();
-        }
-
-        private void BuildingDescCheck(bool enableDesc)
-        {
-            ModConfig.Instance.EnableDescriptions = enableDesc;
-            ModConfig.Instance.Save();
+            helper.AddCheckbox("Enable random building names", ModConfig.Instance.EnableNames, enableName =>
+            {
+                ModConfig.Instance.EnableNames = enableName;
+                ModConfig.Instance.Save();
+            });
+            helper.AddCheckbox("Enable random building descriptions", ModConfig.Instance.EnableDescriptions, enableDesc =>
+            {
+                ModConfig.Instance.EnableDescriptions = enableDesc;
+                ModConfig.Instance.Save();
+            });
         }
     }
 }
