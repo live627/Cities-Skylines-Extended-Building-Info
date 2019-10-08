ae3f17629ef80bf6427565e6d7f62940ad1bb64a
John Rayes <live627@gmail.com>
2019-10-08 13:41:23 -0700

    FindBuildingType

--- ExtendedBuildings/Panels/ServiceInfoWindow.cs
+++ ExtendedBuildings/Panels/ServiceInfoWindow.cs
@@ -40,85 +40,81 @@ namespace ExtendedBuildings
             var buildingId = GetParentInstanceId().Building;
             if (enabled && info.isVisible && BuildingManager.instance != null && ((SimulationManager.instance.m_currentFrameIndex & 15u) == 15u || lastSelected != buildingId))
             {
-                strs = new string[] { "", "", "", "", "", "", "", "" };
+                strs = new string[] { Environment.NewLine, "", "", "", "", "" };
                 lastSelected = buildingId;
                 Building data = BuildingManager.instance.m_buildings.m_buffer[buildingId];
                 var productionRate = PlayerBuildingAI.GetProductionRate(data.m_productionRate, EconomyManager.instance.GetBudget(data.Info.m_class));
                 if (data.m_fireIntensity != 0)
                     productionRate = 0;
-
-                BuildingAI ai = data.Info.m_buildingAI;
-                if (ai is FireStationAI)
-                {
-                    var strength = ((FireStationAI)ai).m_fireDepartmentAccumulation * productionRate / 100;
-                    strs[4] = Localization.Get(LocalizationCategory.ServiceInfo, "FireFighting") + ": " + strength.ToString();
-                    var radius = ((FireStationAI)ai).m_fireDepartmentRadius / 8;
-                    strs[5] = Localization.Get(LocalizationCategory.ServiceInfo, "Radius") + ": " + radius.ToString("F0");
-                    strs[6] = Localization.Get(LocalizationCategory.ServiceInfo, "KittensSaved") + ": " + GetLlamaSightings(1.4);
-                }
-                else if (ai is MonumentAI)
-                {
-                    var strength = ((MonumentAI)ai).m_entertainmentAccumulation * productionRate / 100;
-                    strs[4] = Localization.Get(LocalizationCategory.ServiceInfo, "Entertainment") + ": " + strength.ToString();
-                    var radius = ((MonumentAI)ai).m_entertainmentRadius / 8;
-                    strs[5] = Localization.Get(LocalizationCategory.ServiceInfo, "Radius") + ": " + radius.ToString("F0");
-                    var tourism = ((MonumentAI)ai).m_attractivenessAccumulation * productionRate / 100;
-                    strs[6] = Localization.Get(LocalizationCategory.ServiceInfo, "Attractiveness") + ": " + tourism.ToString();
-                }
-                else if (ai is HospitalAI)
-                {
-                    var strength = ((HospitalAI)ai).m_healthCareAccumulation * productionRate / 100;
-                    strs[4] = Localization.Get(LocalizationCategory.ServiceInfo, "Healthcare") + ": " + strength.ToString();
-                    var radius = ((HospitalAI)ai).m_healthCareRadius / 8;
-                    strs[5] = Localization.Get(LocalizationCategory.ServiceInfo, "Radius") + ": " + radius.ToString("F0");
-                }
-                else if (ai is CemeteryAI)
-                {
-                    var strength = ((CemeteryAI)ai).m_deathCareAccumulation * productionRate / 100;
-                    strs[4] = Localization.Get(LocalizationCategory.ServiceInfo, "Deathcare") + ": " + strength.ToString();
-                    var radius = ((CemeteryAI)ai).m_deathCareRadius / 8;
-                    strs[5] = Localization.Get(LocalizationCategory.ServiceInfo, "Radius") + ": " + radius.ToString("F0");
-                }
-                else if (ai is ParkAI)
-                {
-                    var strength = (int)(((ParkAI)ai).m_entertainmentAccumulation * productionRate / 100);
-                    strs[4] = Localization.Get(LocalizationCategory.ServiceInfo, "Entertainment") + ": " + strength.ToString();
-                    var radius = (int)(((ParkAI)ai).m_entertainmentRadius / 8);
-                    strs[5] = Localization.Get(LocalizationCategory.ServiceInfo, "Radius") + ": " + radius.ToString();
-                    strs[6] = Localization.Get(LocalizationCategory.ServiceInfo, "Llamas") + ": " + GetLlamaSightings(2);
-                }
-                else if (ai is SchoolAI)
-                {
-                    var strength = ((SchoolAI)ai).m_educationAccumulation * productionRate / 100;
-                    strs[4] = Localization.Get(LocalizationCategory.ServiceInfo, "Education") + ": " + strength.ToString();
-                    var radius = ((SchoolAI)ai).m_educationRadius / 8;
-                    strs[5] = Localization.Get(LocalizationCategory.ServiceInfo, "Radius") + ": " + radius.ToString("F0");
-                    strs[6] = Localization.Get(LocalizationCategory.ServiceInfo, "ClassesSkipped") + ": " + GetLlamaSightings(2);
-                }
-                else if (ai is PoliceStationAI)
-                {
-                    var strength = ((PoliceStationAI)ai).m_policeDepartmentAccumulation * productionRate / 100;
-                    strs[4] = Localization.Get(LocalizationCategory.ServiceInfo, "Police") + ": " + strength.ToString();
-                    var radius = ((PoliceStationAI)ai).m_policeDepartmentRadius / 8;
-                    strs[5] = Localization.Get(LocalizationCategory.ServiceInfo, "Radius") + ": " + radius.ToString("F0");
-                }
-                else if (ai is CargoStationAI)
-                {
-                    var strength = ((CargoStationAI)ai).m_cargoTransportAccumulation * productionRate / 100;
-                    strs[4] = Localization.Get(LocalizationCategory.ServiceInfo, "Cargo") + ": " + strength.ToString();
-                    var radius = ((CargoStationAI)ai).m_cargoTransportRadius / 8;
-                    strs[5] = Localization.Get(LocalizationCategory.ServiceInfo, "Radius") + ": " + radius.ToString("F0");
-                }
-
-                //TODO calculate the best relative position for label1
-                label1.text = string.Join(Environment.NewLine, strs);
+                FindBuildingType(data, productionRate);
+                label1.text = String.Format(strs[5] == ""
+                    ? "{0}{0}{0}{1} : {2}{0}{3} : {4}"
+                    : "{0}{0}{0}{1} : {2}{0}{3} : {4}{0}{5}", strs);
             }
         }
 
-        private string GetLlamaSightings(double scale)
+        private void FindBuildingType(Building data, int productionRate)
         {
-            return ((SimulationManager.instance.m_currentGameTime.DayOfYear * scale + GetParentInstanceId().Building) / 1000).ToString("F0");
+            switch (data.Info.m_buildingAI)
+            {
+                case FireStationAI ai:
+                    strs[2] = (ai.m_fireDepartmentAccumulation * productionRate / 100).ToString();
+                    strs[1] = Localization.Get(LocalizationCategory.ServiceInfo, "FireFighting");
+                    strs[4] = (ai.m_fireDepartmentRadius / 8).ToString("F0");
+                    strs[3] = Localization.Get(LocalizationCategory.ServiceInfo, "Radius");
+                    strs[5] = Localization.Get(LocalizationCategory.ServiceInfo, "KittensSaved") + ": " + GetLlamaSightings(1.4);
+                    break;
+                case MonumentAI ai:
+                    strs[2] = (ai.m_entertainmentAccumulation * productionRate / 100).ToString();
+                    strs[1] = Localization.Get(LocalizationCategory.ServiceInfo, "Entertainment");
+                    strs[4] = (ai.m_entertainmentRadius / 8).ToString("F0");
+                    strs[3] = Localization.Get(LocalizationCategory.ServiceInfo, "Radius");
+                    var tourism = ai.m_attractivenessAccumulation * productionRate / 100;
+                    strs[5] = Localization.Get(LocalizationCategory.ServiceInfo, "Attractiveness") + ": " + tourism.ToString();
+                    break;
+                case HospitalAI ai:
+                    strs[2] = (ai.m_healthCareAccumulation * productionRate / 100).ToString();
+                    strs[1] = Localization.Get(LocalizationCategory.ServiceInfo, "Healthcare");
+                    strs[4] = (ai.m_healthCareRadius / 8).ToString("F0");
+                    strs[3] = Localization.Get(LocalizationCategory.ServiceInfo, "Radius");
+                    break;
+                case CemeteryAI ai:
+                    strs[2] = (ai.m_deathCareAccumulation * productionRate / 100).ToString();
+                    strs[1] = Localization.Get(LocalizationCategory.ServiceInfo, "Deathcare");
+                    strs[4] = (ai.m_deathCareRadius / 8).ToString("F0");
+                    strs[3] = Localization.Get(LocalizationCategory.ServiceInfo, "Radius");
+                    break;
+                case ParkAI ai:
+                    strs[2] = (ai.m_entertainmentAccumulation * productionRate / 100).ToString();
+                    strs[1] = Localization.Get(LocalizationCategory.ServiceInfo, "Entertainment");
+                    strs[4] = (ai.m_entertainmentRadius / 8).ToString("F0");
+                    strs[3] = Localization.Get(LocalizationCategory.ServiceInfo, "Radius");
+                    strs[5] = Localization.Get(LocalizationCategory.ServiceInfo, "Llamas") + ": " + GetLlamaSightings(2);
+                    break;
+                case SchoolAI ai:
+                    strs[2] = (ai.m_educationAccumulation * productionRate / 100).ToString();
+                    strs[1] = Localization.Get(LocalizationCategory.ServiceInfo, "Education");
+                    strs[4] = (ai.m_educationRadius / 8).ToString("F0");
+                    strs[3] = Localization.Get(LocalizationCategory.ServiceInfo, "Radius");
+                    strs[5] = Localization.Get(LocalizationCategory.ServiceInfo, "ClassesSkipped") + ": " + GetLlamaSightings(2);
+                    break;
+                case PoliceStationAI ai:
+                    strs[2] = (ai.m_policeDepartmentAccumulation * productionRate / 100).ToString();
+                    strs[1] = Localization.Get(LocalizationCategory.ServiceInfo, "Police");
+                    strs[4] = (ai.m_policeDepartmentRadius / 8).ToString("F0");
+                    strs[3] = Localization.Get(LocalizationCategory.ServiceInfo, "Radius");
+                    break;
+                case CargoStationAI ai:
+                    strs[2] = (ai.m_cargoTransportAccumulation * productionRate / 100).ToString();
+                    strs[1] = Localization.Get(LocalizationCategory.ServiceInfo, "Cargo");
+                    strs[4] = (ai.m_cargoTransportRadius / 8).ToString("F0");
+                    strs[3] = Localization.Get(LocalizationCategory.ServiceInfo, "Radius");
+                    break;
+            }
         }
+
+        private string GetLlamaSightings(double scale) =>
+            ((SimulationManager.instance.m_currentGameTime.DayOfYear * scale + GetParentInstanceId().Building) / 1000).ToString("F0");
 
         private InstanceID GetParentInstanceId()
         {
