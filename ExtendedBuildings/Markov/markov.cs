b55d0d9c51cc13fb45ac2bbf5ee9ebae648c52c0
John Rayes <live627@gmail.com>
2019-11-11 02:44:35 -0700

    ternaries for markov

--- ExtendedBuildings/Markov/markov.cs
+++ ExtendedBuildings/Markov/markov.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using ColossalFramework.Math;
@@ -9,198 +9,130 @@ namespace ExtendedBuildings
     {
         private Dictionary<string, Ngram> pairs = new Dictionary<string, Ngram>();
         private Dictionary<string, int> starters = new Dictionary<string, int>();
-        int n;
-        bool isWord;
+        readonly int n;
+        readonly bool isWord;
 
         public Markov(string resourceName, bool useWords, int n)
         {
-            var resource = Localization.Get(LocalizationCategory.Markov, resourceName);           
+            var buffer = Localization.GetResource(resourceName);           
             this.n = n;
-
-            var buffer = resource.Split(new string[] { "\r\n","\n" }, StringSplitOptions.None);
-            foreach (var line in buffer)
-                if (line != null || line.Trim().Length > n)
+            isWord = useWords;
+            for (int i = 0; i < buffer.Count; i++)
+                if (buffer[i].Length > n)
                 {
                     if (useWords)
-                        GenerateByWord(line.Trim(), n);
+                        GenerateByWord(buffer[i].Trim());
                     else
-                        GenerateByChar(line.Trim(), n);
+                        GenerateByChar(buffer[i].Trim());
                 }
         }
 
-
-        private void GenerateByWord(string p, int n)
+        private void GenerateByWord(string p)
         {
-            isWord = true;
             var line = p.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             if (line.Length < n)
-            {
                 return;
-            }
 
             var result = new string[n];
             for (var i = 0; i < line.Length - n; i += 1)
             {
                 for (var j = 0; j < n; j += 1)
-                {
                     result[j] = line[i + j];
-                }
+
                 var rr = String.Join(" ", result);
                 if (i == 0)
                 {
                     if (!starters.ContainsKey(rr))
-                    {
                         starters.Add(rr, 0);
-                    }
+
                     starters[rr] += 1;
                 }
 
                 if (!pairs.ContainsKey(rr))
-                {
                     pairs.Add(rr, new Ngram());
-                }
-                if (i + n < line.Length)
-                {
-                    string next = line[i + n];
-                    pairs[rr].Add(next);
-                }
-                else
-                {
-                    pairs[rr].Add("");
-                }
+
+                pairs[rr].Add(i + n < line.Length ? line[i + n] : "");
             }
         }
 
-        private void GenerateByChar(string p, int n)
+        private void GenerateByChar(string p)
         {
-            isWord = false;
             var line = p.ToCharArray();
             if (line.Length < n)
-            {
                 return;
-            }
-
 
             for (var i = 0; i < line.Length - n; i += 1)
             {
                 var result = "";
                 for (var j = 0; j < n; j += 1)
-                {
                     result += line[i + j];
-                }
 
                 if (i == 0)
                 {
                     if (!starters.ContainsKey(result))
-                    {
                         starters.Add(result, 0);
-                    }
+
                     starters[result] += 1;
                 }
 
                 if (!pairs.ContainsKey(result))
-                {
                     pairs.Add(result, new Ngram());
-                }
 
-                if (i + n < line.Length)
-                {
-                    string next = new String(line, i + n, 1);
-                    pairs[result].Add(next);
-                }
-                else
-                {
-                    pairs[result].Add("");
-                }
+                pairs[result].Add(i + n < line.Length ? new String(line, i + n, 1) : "");
             }
         }
 
         public string GetText(int min, int max, bool useStarter)
         {
             var rand = new Randomizer(DateTime.Now.Ticks);
-            var test = rand.Int32(0, 3);
             return GetText(ref rand, min, max, useStarter);
         }
 
-        public string GetText(ref Randomizer rand, int min, int max, bool useStarter,bool endOnSpaces = false)
+        public string GetText(ref Randomizer rand, int min, int max, bool useStarter, bool endOnSpaces = false)
         {
             var what = rand.Int32(41u);
             var result = new List<string>();
             
             while (result.Count() < min)
             {
-                var starter = "";
-                if (useStarter)
-                {
-                    starter = GetRandomStarter(ref rand);
-                }
-                else
-                {
-                    starter = pairs.Keys.ElementAt(rand.Int32((uint)pairs.Count()));
-                }
+                var starter = useStarter ? GetRandomStarter(ref rand) : pairs.Keys.ElementAt(rand.Int32((uint)pairs.Count()));
                 var current = pairs[starter];
 
-                string[] starterSplit;
-                if (isWord)
-                {
-                    starterSplit = starter.Split(' ');
-                    foreach (var ss in starterSplit)
-                    {
-                        result.Add(ss);
-                    }
-                }
-                else
-                {
-                    foreach (var cc in starter.ToCharArray())
-                    {
-                        result.Add(cc.ToString());
-                    }
-                }
+                result.AddRange(isWord ? starter.Split(' ') : starter.ToCharArray().Select(cc => cc.ToString()));
 
                 for (var i = 0; i < 1000; i += 1)
                 {
                     var next = current.GetNext(ref rand);
                     if (next == "")
-                    {
                         break;
-                    }
+
                     result.Add(next);
 
                     var count = result.Count() - n;
                     var nextCurrent = new string[n];
                     for (var j = 0; j < n; j += 1)
-                    {
                         nextCurrent[j] = result[count + j];
-                    }
 
-                    var nc = "";
-                     nc = String.Join(isWord ? " " : "", nextCurrent);
+                    var nc = String.Join(isWord ? " " : "", nextCurrent);
 
                     if (!pairs.ContainsKey(nc))
-                    {
                         break;
-                    }
+
                     current = pairs[nc];
                     var lastWord = result[result.Count - 1];
                     var lastChar = lastWord.Substring(lastWord.Length - 1);
                     if (result.Count > ((max - min) / 2 + min))
                     {
                         var randValue = rand.Int32((uint)(Math.Max(0,max * 2 - result.Count)));
-                        if (endOnSpaces && (lastChar == " " && randValue < 3))
-                        {
+                        if (endOnSpaces && (lastChar == " " && randValue < 3)
+                            || (lastChar == "."|| lastChar == "!") && randValue < 8)
                             break;
-                        }
-                        if ((lastChar == "."|| lastChar == "!") && randValue < 8){
-                            break;
-                        }
                     }
                     
                 }
 
                 if (!isWord)
-                {
                     result.Add(" ");
-                }
             }
 
             return string.Join(isWord ? " " : "", result.ToArray()).Trim();
@@ -216,9 +148,7 @@ namespace ExtendedBuildings
             {
                 sum += kvp.Value;
                 if (sum >= target)
-                {
                     return kvp.Key;
-                }
             }
             return null;
         }
